80fbf0f6cf4c2b90e69b3bbb3d3dc4a430f48eed
djeldo <eldad.donio@gmail.com>
2024-06-14 12:57:24 +0300

    establish proj

---
+++ source/Services/Fleet/Api/FleetApi/Program.cs
@@ -0,0 +1,7 @@
+using FleetApi;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ source/Services/Ship/Api/ShipApi/Program.cs
@@ -0,0 +1,7 @@
+using ShipApi;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
